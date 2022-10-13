d75cbb27d8d821246844ee1fcac08558e5db8e05
raphaelzhou1 <tianyu_zhou1@brown.edu>
2022-10-13 10:56:42 -0400

    Tianyu's Etherscan tool

--- etherscan_tool/etherscan.py
+++ etherscan_tool/etherscan.py
@@ -4,12 +4,10 @@ from json import tool
 
 import requests
 
-from modules import accounts
+import modules.modules as aggregate_modules
 
 import configs
-from enums.enums import ActionsEnum
- 
-# from etherscan.utils.parsing import ResponseParser as parser
+from enums.enums import FieldsEnum as fields
 
 class Etherscan:
     def __new__(cls, api_key: str, net: str = "MAINNET"):
@@ -22,37 +20,47 @@ class Etherscan:
         with open(config_path, "r") as f: # reads and creates a File Object
             return json.load(f) # converts File to JSON object
 
-    # @staticmethod
-    # def __run(func, api_key: str, net: str):
-    #     def wrapper(*args, **kwargs):
-    #         url = (
-    #             f"{fields.PREFIX.format(net.lower()).replace('-main','')}"
-    #             f"{func(*args, **kwargs)}"
-    #             f"{fields.API_KEY}"
-    #             f"{api_key}"
-    #         )
-    #         r = requests.get(url, headers={"User-Agent": ""})
-    #         return parser.parse(r)
-
-    #     return wrapper
+    @staticmethod
+    def __run(func, api_key: str, net: str):
+        def wrapper(*args, **kwargs):
+            url = (
+                f"{fields.PREFIX.format(net.lower()).replace('-main','')}"
+                f"{func(*args, **kwargs)}"
+                f"{fields.API_KEY}"
+                f"{api_key}"
+            )
+            r = requests.get(url, headers={"User-Agent": ""}) # returns a requests.Response object
+            return parser.parse(r)
+        return wrapper
 
     @classmethod
     def from_config(cls, api_key: str, config_path: str, net: str):
         config = cls.__load_config(config_path) # returns JSON object (a dictionary)
         for func, v in config.items(): # each pair in the Dict
-            print(func)
-            print(v["module"])
-
             if not func.startswith("_"):  # disabled if _
-                attr = getattr(getattr(accounts, v["module"]), func)
-                print(attr)
+                attr = getattr(getattr(aggregate_modules, v["module"].capitalize()), func) # aggregate_modules refers to file modules of folder modules; v["module"] returns particular class name in aggregate_modules; func is the static function that should be returned as a function
                 setattr(cls, func, cls.__run(attr, api_key, net))
         return cls
 
-
-# print(Etherscan("TE9HIXVD4MTIGAP9SJB8F3V4E39UTP8C1E"));
+class ResponseParser:
+    @staticmethod
+    def parse(response: requests.Response):
+        content = response.json()
+        result = content["result"]
+        if "status" in content.keys():
+            status = bool(int(content["status"]))
+            message = content["message"]
+            assert status, f"{result} -- {message}"
+        else:
+            # GETH or Parity proxy msg format
+            # TODO: see if we need those values
+            jsonrpc = content["jsonrpc"]
+            cid = int(content["id"])
+        return result
 
 def main():
-    Etherscan("TE9HIXVD4MTIGAP9SJB8F3V4E39UTP8C1E")
+    etherscan = Etherscan("TE9HIXVD4MTIGAP9SJB8F3V4E39UTP8C1E")
+    print(etherscan.get_eth_balance("TE9HIXVD4MTIGAP9SJB8F3V4E39UTP8C1E"))
 
+# print(test_run("<function Proxy.get_proxy_block_number at 0x105f1e560>", "TE9HIXVD4MTIGAP9SJB8F3V4E39UTP8C1E", "MAINNET"))
 main()
